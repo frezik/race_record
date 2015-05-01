335ae37c5cec6dae84edcb4802574cbfe448edf6
Timm Murray <tmurray@wumpus-cave.net>
2015-05-01 14:36:47 -0500

    Launch video after GPS and Accel programs in the hope of getting the data recording going faster

--- listener.sh
+++ listener.sh
@@ -18,12 +18,16 @@ do
     FILE_PREFIX="${OUTPUT_DIR}/record_${TIME}"
 
     # Launch data recording programs
-    ./record_video.pl --vid-file="${FILE_PREFIX}.avi" --data-file="${FILE_PREFIX}_vid_data.json" &
-    VID_PID=$!
     ./record_gps.pl --data-file="${FILE_PREFIX}_gps_data.json" &
     GPS_PID=$!
     ./record_accel.pl --data-file="${FILE_PREFIX}_accel_data.json" &
     ACCEL_PID=$!
+    # This gives the GPS and accel recorders a chance to get going before 
+    # we start the Big One
+    sleep 2
+    ./record_video.pl --vid-file="${FILE_PREFIX}.avi" --data-file="${FILE_PREFIX}_vid_data.json" &
+    VID_PID=$!
+
 
     # Turn on LED
     gpio -g write ${LED_PIN} 1
@@ -33,9 +37,9 @@ do
     # Wait for switch to change state
     gpio -g wfi ${SWITCH_PIN} falling
     # Kill data recording programs
-    kill ${VID_PID}
     kill ${GPS_PID}
     kill ${ACCEL_PID}
+    kill ${VID_PID}
     # Turn off LED
     gpio -g write ${LED_PIN} 0
     sleep 2
