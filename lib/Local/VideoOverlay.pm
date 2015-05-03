67563c7a603bea2e7015aebe258a319e2af825cf
tmurray <tmurray@wumpus-cave.net>
2015-05-03 10:12:16 -0500

    Start on video frame overlay

---
+++ lib/Local/VideoOverlay.pm
@@ -0,0 +1,88 @@
+# Copyright (c) 2015  Timm Murray
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without 
+# modification, are permitted provided that the following conditions are met:
+# 
+#     * Redistributions of source code must retain the above copyright notice, 
+#       this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright 
+#       notice, this list of conditions and the following disclaimer in the 
+#       documentation and/or other materials provided with the distribution.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+# POSSIBILITY OF SUCH DAMAGE.
+package Local::VideoOverlay;
+use v5.20;
+use warnings;
+use Moose::Role;
+use Imager;
+
+
+has width => (
+    is       => 'ro',
+    isa      => 'Int',
+    required => 1,
+);
+has height => (
+    is       => 'ro',
+    isa      => 'Int',
+    required => 1,
+);
+has accel_x => (
+    is       => 'ro',
+    isa      => 'Num',
+    required => 1,
+);
+has accel_y => (
+    is       => 'ro',
+    isa      => 'Num',
+    required => 1,
+);
+has accel_z => (
+    is       => 'ro',
+    isa      => 'Num',
+    required => 1,
+);
+has gps_lat => (
+    is       => 'ro',
+    isa      => 'Num',
+    required => 1,
+);
+has gps_lat_ns => (
+    is       => 'ro',
+    isa      => Moose::enum([qw{ n s }]),
+    required => 1,
+);
+has gps_long => (
+    is       => 'ro',
+    isa      => 'Num',
+    required => 1,
+);
+has gps_long_ew => (
+    is       => 'ro',
+    isa      => Moose::enum([qw{ e w }]),
+    required => 1,
+);
+has gps_kph => (
+    is       => 'ro',
+    isa      => 'Num',
+    required => 1,
+);
+
+
+requires 'make_frame';
+
+
+1;
+__END__
+
---
+++ make_frame.pl
@@ -0,0 +1,49 @@
+#!perl
+# Copyright (c) 2015  Timm Murray
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without 
+# modification, are permitted provided that the following conditions are met:
+# 
+#     * Redistributions of source code must retain the above copyright notice, 
+#       this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright 
+#       notice, this list of conditions and the following disclaimer in the 
+#       documentation and/or other materials provided with the distribution.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+# POSSIBILITY OF SUCH DAMAGE.
+use v5.20;
+use warnings;
+use lib 'lib';
+use Local::VideoOverlay::SimpleAccel;
+
+
+my $OUT_FILE = shift or die "Need output file\n";
+
+my $over = Local::VideoOverlay::SimpleAccel->new({
+    width       => 800,
+    height      => 600,
+    accel_x     => 0.75,
+    accel_y     => 0,
+    accel_z     => 0,
+    gps_lat     => 0,
+    gps_lat_ns  => 'n',
+    gps_long    => 0,
+    gps_long_ew => 'w',
+    gps_kph     => 25,
+});
+my $img = $over->make_frame;
+$img->write(
+    file => $OUT_FILE,
+    type => 'png',
+);
