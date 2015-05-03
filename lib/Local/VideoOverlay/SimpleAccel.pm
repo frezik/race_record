f7e31d67e082346914721c0f03fcdcafd4487a56
tmurray <tmurray@wumpus-cave.net>
2015-05-03 10:44:30 -0500

    Draw a simple acceleration overlay

--- lib/Local/VideoOverlay/SimpleAccel.pm
+++ lib/Local/VideoOverlay/SimpleAccel.pm
@@ -29,6 +29,17 @@ use namespace::autoclean;
 
 with 'Local::VideoOverlay';
 
+use constant DEBUG => 1;
+
+use constant ACCEL_PERCENT_WIDTH           => 22;
+use constant ACCEL_POSITION_HEIGHT_PERCENT => 90;
+use constant ACCEL_COLOR_R                 => 237;
+use constant ACCEL_COLOR_B                 => 31;
+use constant ACCEL_COLOR_G                 => 141;
+use constant OVERLAY_BACKGROUND_COLOR_R    => 216;
+use constant OVERLAY_BACKGROUND_COLOR_G    => 216;
+use constant OVERLAY_BACKGROUND_COLOR_B    => 199;
+
 
 sub make_frame
 {
@@ -36,12 +47,61 @@ sub make_frame
     my $width   = $self->width;
     my $height  = $self->height;
     my $accel_x = $self->accel_x;
+    my $img     = $self->_init_img( $width, $height );
+
+    my $center_x     = int( $width / 2 );
+    my $line_size    = int( $width * (ACCEL_PERCENT_WIDTH / 100) );
+    my $line_start_x = $center_x - ($line_size / 2);
+    my $line_end_x   = $line_start_x + $line_size;
+    my $line_y       = int( $height * (ACCEL_POSITION_HEIGHT_PERCENT / 100) );
+    say "Drawing at ($line_start_x, $line_y) to ($line_end_x, $line_y)" if DEBUG;
+
+    my $overlay_background_color = Imager::Color->new(
+        OVERLAY_BACKGROUND_COLOR_R, OVERLAY_BACKGROUND_COLOR_G,
+        OVERLAY_BACKGROUND_COLOR_B, 0xFF );
+    $img->box(
+        color  => $overlay_background_color,
+        filled => 1,
+        xmin   => $line_start_x - 2,
+        ymin   => $line_y - 9,
+        xmax   => $line_end_x + 2,
+        ymax   => $line_y + 2,
+    );
+    my $accel_color = Imager::Color->new(
+        ACCEL_COLOR_R, ACCEL_COLOR_G, ACCEL_COLOR_B, 0xFF );
+    $img->box(
+        color  => $accel_color,
+        filled => 1,
+        xmin   => $line_start_x,
+        ymin   => $line_y - 1,
+        xmax   => $line_end_x,
+        ymax   => $line_y + 1,
+    );
+
+    my $line_half_width = $line_size / 2;
+    my $indicator_x = $center_x + ($line_half_width * $accel_x);
+    say "Drawing indicator at ($indicator_x, $line_y)" if DEBUG;
+    $img->line(
+        color => $accel_color,
+        x1    => $indicator_x,
+        y1    => $line_y,
+        x2    => $indicator_x,
+        y2    => $line_y - 7,
+    );
+
+    return $img;
+}
+
+sub _init_img
+{
+    my ($self, $width, $height) = @_;
 
     my $img = Imager->new(
         xsize    => $width,
         ysize    => $height,
         channels => 4,
-    );
+    ) or die Imager->errstr;
+
     my $transparent = Imager::Color->new( 0xFF, 0xFF, 0xFF, 0 );
     $img->flood_fill( x => 1, y => 1, color => $transparent );
 
