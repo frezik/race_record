604eefafc33539a76c31d22482691ea42ec1c7f2
tmurray <tmurray@wumpus-cave.net>
2015-05-12 22:02:26 -0500

    Document the major scripts in README. Remove done TODO items, add new ones.

--- combine_overlay.sh
+++ combine_overlay.sh
@@ -3,4 +3,4 @@ INPUT_VID=$1
 OVERLAY_VID=$2
 OUTPUT_VID=$3
 
-ffmpeg -i ${INPUT_VID} -i ${OVERLAY_VID} -filter_complex 'overlay=format=rgb' ${OUTPUT_VID}
+ffmpeg -i ${INPUT_VID} -i ${OVERLAY_VID} -filter_complex 'overlay=format=rgb' -b:v 8k ${OUTPUT_VID}
